7b717f88bf05853d7f53294d863b316eebe85bbb
temka <omfgtemuujin@gmail.com>
2020-04-27 02:10:22 +0800

    shoo shideh

--- app.js
+++ app.js
@@ -8,11 +8,20 @@ var onoo = [0, 0];
 var roundOnoo = 0;
 
 //shoonii ali talaaraa buusang hadgalah
-var shoo = Math.floor(Math.random() * 6) + 1;
-// <div class="player-score" id="score-0">43</div>
-window.document.querySelector("#score-0").textContent = 0;
-window.document.querySelector("#score-1").textContent = 0;
-window.document.querySelector("#current-0").textContent = 0;
-window.document.querySelector("#current-1").textContent = 0;
-document.querySelector(".dice").style.display = "none";
-console.log("Shoo: " + shoo);
+var shooNumber = Math.floor(Math.random() * 6) + 1;
+
+// ehelel beltgel
+
+document.getElementById("score-0").textContent = 0;
+document.getElementById("score-1").textContent = 0;
+
+document.getElementById("current-0").textContent = 0;
+document.getElementById("current-1").textContent = 0;
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
+
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  var shooNumber = Math.floor(Math.random() * 6) + 1;
+  diceDom.style.display = "block";
+  diceDom.src = "dice-" + shooNumber + ".png";
+});
